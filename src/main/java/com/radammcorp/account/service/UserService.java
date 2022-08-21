223f79552a3c834fcdcaf35e9920e12cbfd3b44e
Ashwin R <ashwin.bittu@gmail.com>
2022-08-21 20:28:36 +1000

    all changes

---
+++ src/main/java/com/radammcorp/account/service/UserService.java
@@ -0,0 +1,11 @@
+package com.radammcorp.account.service;
+
+import com.radammcorp.account.model.User;
+
+/** {@author waheedk}!*/
+public interface UserService {
+	/** {@inheritDoc}} !*/
+    void save(User user);
+    /** {@inheritDoc}} !*/
+    User findByUsername(String username);
+}
